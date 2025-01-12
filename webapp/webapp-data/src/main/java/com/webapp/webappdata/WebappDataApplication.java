cd6eee3b244f47138774bd9800bcb3af844e1159
oemak <oemak91@gmail.com>
2025-01-12 02:21:31 +0100

    first commit

---
+++ webapp/webapp-data/src/main/java/com/webapp/webappdata/WebappDataApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.webappdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappDataApplication.class, args);
+	}
+
+}
